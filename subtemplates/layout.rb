363bed3f724f440d0d98ce52fe04c93820b7105e
Samuel Oey <oey@ubilabs.net>
2011-03-23 10:23:09 +0100

    fixes indention for layout.html.haml

--- subtemplates/layout.rb
+++ subtemplates/layout.rb
@@ -18,13 +18,13 @@ inside "app/views/layouts" do
     # gsub_file "application.html.haml", /\/ insert javascript here\n/, "= include_javascripts :application\n" if @use_jammit.present?
     # gsub_file "application.html.haml", /\/ insert javascript here\n/, "= javascript_include_tag :defaults\n" if @use_jammit.nil?
     inject_into_file "application.html.haml", :after => "/ insert javascript here\n" do
-      @use_jammit ? "= include_javascripts :application\n" : "= javascript_include_tag :defaults\n"
+      @use_jammit ? "#{' ' * 4}= include_javascripts :application\n" : "#{' ' * 4}= javascript_include_tag :defaults\n"
     end
 
     # gsub_file "application.html.haml", /\/ insert stylesheet here\n/, "= include_stylesheets :application\n"  if @use_jammit.present?
     # gsub_file "application.html.haml", /\/ insert stylesheet here\n/, "= stylesheet_link_tag 'reset'\n"       if @use_jammit.nil?    
     inject_into_file "application.html.haml", :after => "/ insert stylesheet here\n" do
-      @use_jammit ? "= include_stylesheets :application\n" : "= stylesheet_link_tag 'reset'\n"
+      @use_jammit ? "#{' ' * 4}= include_stylesheets :application\n" : "#{' ' * 4}= stylesheet_link_tag 'reset'\n"
     end
 
   else
