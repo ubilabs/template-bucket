fff51827b084f1f7d924f0f20ad1b7bf1d6eaf8b
Samuel Oey <oey@ubilabs.net>
2011-03-23 10:23:40 +0100

    adds rails admin gem

--- subtemplates/gems.rb
+++ subtemplates/gems.rb
@@ -2,7 +2,7 @@ gem 'rails3-generators'
 
 @gem_template_path = File.dirname(__FILE__)+"/gems"
 
-gem_templates= %w{haml jammit devise formtastic cancan email_spec inherited_resources kaminari watchr exception_notification}
+gem_templates= %w{haml jammit devise formtastic cancan email_spec inherited_resources kaminari watchr exception_notification rails_admin}
 
 gem_templates.each do |template|
   if yes?("Apply Template for #{template}?", :yellow)
